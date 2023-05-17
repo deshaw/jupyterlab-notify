c8e03b4864304fb6050ca4df489bf6a41abf980b
Peyton Murray <peynmurray@gmail.com>
2023-05-16 23:26:34 -0700

    Upgrade to jupyterlab 4 (#1)

--- jupyterlab_notify/__init__.py
+++ jupyterlab_notify/__init__.py
@@ -1,20 +1,21 @@
-import json
-from pathlib import Path
 from .magics import NotifyCellCompletionMagics
 from ._version import __version__
 
-HERE = Path(__file__).parent.resolve()
-
-with (HERE / "labextension" / "package.json").open() as fid:
-    data = json.load(fid)
-
 
 def _jupyter_labextension_paths():
-    return [{"src": "labextension", "dest": data["name"]}]
+    return [{
+        "src": "labextension",
+        "dest": "jupyterlab-notify"
+    }]
+
+
+def _jupyter_server_extension_points():
+    return [{
+        "module": "jupyterlab_notify"
+    }]
 
 
 def _load_jupyter_server_extension(server_app):
-    # Nothing to do for now
     pass
 
 
